0bc99db16047ec77787bacb775c5f603b054f129
Nam Ckiku <tranhoangnam11373@gmail.com>
2016-01-28 16:33:33 +0700

    Add project

---
+++ NamCkiku/NamCkiku.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NamCkiku.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ NamCkiku/NamCkiku/Areas/Administrator/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NamCkiku.Areas.Administrator.Controllers
+{
+    public class ProductController : Controller
+    {
+        public ActionResult ViewProduct()
+        {
+            return View();
+        }
+	}
+}
